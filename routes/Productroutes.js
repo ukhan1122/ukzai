9e97e337f7e7d2c84c56fcc153c5b262a22ff041
ukhan1122 <143578110+ukhan1122@users.noreply.github.com>
2026-06-16 18:06:38 +0500

    Update Productroutes.js
    update

--- routes/Productroutes.js
+++ routes/Productroutes.js
@@ -1,16 +1,16 @@
-const express                                                        = require("express");
-const upload                                                         = require("../config/cloudinary");
-const { createProduct, getAllProducts, getProductById, updateProduct, deleteProduct } = require("../controllers/productController");
-const { authMiddleware, adminMiddleware }                            = require("../middleware/auth");
+const express = require("express");
+const upload = require("../config/cloudinary");
+const { createProduct, getAllProducts, getProductById, updateProduct, deleteProduct } = require("../controllers/Productcontroller");
+const { authMiddleware, adminMiddleware } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.get("/",    getAllProducts);
+router.get("/", getAllProducts);
 router.get("/:id", getProductById);
 
 // Admin only
-router.post("/",    authMiddleware, adminMiddleware, upload.array("images", 5), createProduct);
-router.put("/:id",  authMiddleware, adminMiddleware, upload.array("images", 5), updateProduct);
+router.post("/", authMiddleware, adminMiddleware, upload.array("images", 5), createProduct);
+router.put("/:id", authMiddleware, adminMiddleware, upload.array("images", 5), updateProduct);
 router.delete("/:id", authMiddleware, adminMiddleware, deleteProduct);
 
 module.exports = router;
